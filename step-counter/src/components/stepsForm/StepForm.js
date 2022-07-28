6a1940dad646baf50ba9fe4e0c7acd8f3a11b7bf
Samip <callmesamip@gmail.com>
2022-07-28 13:03:12 +1000

    The styling for the textbox is now okay

--- step-counter/src/components/stepsForm/StepForm.js
+++ step-counter/src/components/stepsForm/StepForm.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
-
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import './StepForm.css';
 export const StepForm = () => {
-	return <Form className="isSubmitForm">ksdnfhkashdfk</Form>;
+	return (
+		<Container className="px-4">
+			<div className="hasSubmitForm">
+				<hr className="isHR mb-2 mt-2" />
+				<Form className="isSubmitForm p-4">
+					<Row className="g-3">
+						<Col sm={12} md={3}>
+							<input type="Date" className="isTextBox p-4"></input>
+						</Col>
+						<Col sm={12} md={9} className="d-flex justify-content-end isCol">
+							<input type="text" className="isTextBox p-4 "></input>
+							<Button type="submit" className="mx-2 isSubmitButton">
+								Submit
+							</Button>
+						</Col>
+						<hr className="isHR mt-3" />
+					</Row>
+				</Form>
+			</div>
+		</Container>
+	);
 };
