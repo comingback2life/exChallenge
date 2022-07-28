c0b2a9e08b7d5948c8157e38c4160662bbc4718b
Samip <callmesamip@gmail.com>
2022-07-28 13:51:58 +1000

    Figured out the table styling, now onto populating data

--- step-counter/src/components/stepsForm/StepsTable.js
+++ step-counter/src/components/stepsForm/StepsTable.js
@@ -9,16 +9,16 @@ export const StepsTable = () => {
 				{/**Since I am only using one style for the table isBordered class can be found in the StepForm.css */}
 				<tbody>
 					<tr>
-						<td>1</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
+						<td className="p-3">1</td>
+						<td className="p-3">Mark</td>
+						<td className="p-3">Otto</td>
+						<td className="p-3">@mdo</td>
 					</tr>
 					<tr>
-						<td>2</td>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
+						<td className="p-3">1</td>
+						<td className="p-3">Mark</td>
+						<td className="p-3">Otto</td>
+						<td className="p-3">@mdo</td>
 					</tr>
 				</tbody>
 			</Table>
