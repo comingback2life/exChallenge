cacda78c0a6a06f29c8ef3dafabfb94363e8619e
Samip <callmesamip@gmail.com>
2022-07-28 14:20:38 +1000

    Styling completed, now want to display the data

--- step-counter/src/components/stepsForm/StepsTable.js
+++ step-counter/src/components/stepsForm/StepsTable.js
@@ -5,23 +5,29 @@ import './StepForm.css';
 export const StepsTable = () => {
 	return (
 		<Container>
-			<Table variant="none" className="isBordered">
+			<Table variant="none" className="isBordered" responsive>
 				{/**Since I am only using one style for the table isBordered class can be found in the StepForm.css */}
 				<tbody>
 					<tr>
-						<td className="p-3">1</td>
-						<td className="p-3">Mark</td>
-						<td className="p-3">Otto</td>
-						<td className="p-3">@mdo</td>
+						<td className="py-3">1</td>
+						<td className="py-3 text-start" colSpan={3}>
+							Date
+						</td>
+						<td className="py-3 text-end">Stepstaken</td>
 					</tr>
 					<tr>
-						<td className="p-3">1</td>
-						<td className="p-3">Mark</td>
-						<td className="p-3">Otto</td>
-						<td className="p-3">@mdo</td>
+						<td className="py-3">1</td>
+						<td className="py-3 text-start">Date</td>
+						<td className="py-3 text-end" colSpan={5}>
+							Stepstaken
+						</td>
 					</tr>
 				</tbody>
 			</Table>
+			<div className="d-flex justify-content-between">
+				<p className="px-5"> Total</p>
+				<p className="px-4"> 10</p>
+			</div>
 		</Container>
 	);
 };
