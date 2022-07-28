537f53763239d6faf152a3671f124d74ed1e82df
Samip <callmesamip@gmail.com>
2022-07-29 01:39:33 +1000

    Unused Code Cleanup

--- step-counter/src/components/helpers/axiosHelper.js
+++ step-counter/src/components/helpers/axiosHelper.js
@@ -4,20 +4,18 @@ import { useState } from 'react';
 const stepEP = process.env.REACT_APP_STEP_EP;
 
 export const fetchSteps = async () => {
-	const dates = [];
 	let totalSteps = 0;
 	const { data } = await axios.get(stepEP);
 	const { result } = data;
 	for (let i in result) {
 		totalSteps = totalSteps + result[i].stepsTaken;
-		dates.push(result[i].dateOfStep);
 	}
 	//(a, b) => (dayjs(a).isAfter(dayjs(b)) ? 1 : -1)
 	const sortedByDate = result.sort((a, b) =>
 		dayjs(a.dateOfStep).isAfter(dayjs(b.dateOfStep)) ? 1 : -1
 	);
 	data.result = sortedByDate.reverse();
-	console.log(data.result, 'afterSorting');
+
 	const newDataObj = {
 		...data,
 		totalSteps,
